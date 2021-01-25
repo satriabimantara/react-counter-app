a4a47958a1d2e6949a4528fc127007707fc4862b
satriabimantara <satriabimantara.md@gmail.com>
2021-01-25 23:25:24 +0800

    Make Counters component to handle a counter component

---
+++ src/components/Counters.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handlingDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handlingDelete}
+            counter={counter}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
