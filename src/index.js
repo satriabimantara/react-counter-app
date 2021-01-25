2234e3093d1310e7606e8cfc6528fcb017e26540
satriabimantara <satriabimantara.md@gmail.com>
2021-01-25 22:34:20 +0800

    Initialize bootstrap modules and importing Counter App in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/Counter'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
