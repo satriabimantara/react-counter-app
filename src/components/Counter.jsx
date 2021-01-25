cd4ed7c55d2055ff56c29c8f4e998266cadf9a6f
satriabimantara <satriabimantara.md@gmail.com>
2021-01-25 23:25:53 +0800

    Adding delete button and delete event handler in counters.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 10,
+    count: this.props.counter.value,
     tags_element: ["tag1", "tag2", "tag3", "tag4"],
   };
 
@@ -48,9 +48,17 @@ class Counter extends Component {
           onClick={this.handlingDecrement}
           className="btn btn-danger btn-sm ml-3"
         >
-          Increment
+          Decrement
         </button>
-        {this.renderTags()}
+        <button
+          className="btn btn-sm btn-success m-2"
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
+        >
+          Delete
+        </button>
+        {/* this.renderTags() */}
       </div>
     );
   }
