b78e585ff8112a72920c7f24930d923f77ac544d
satriabimantara <satriabimantara.md@gmail.com>
2021-01-26 21:38:27 +0800

    Memindahkan state dan all method dari Counters into App root

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -1,59 +1,22 @@
 import React, { Component } from "react";
 import Counter from "./Counter";
+
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-  handlingIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = counter;
-    counters[index].value++;
-    this.setState({ counters: counters });
-  };
-  handlingDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = counter;
-    counters[index].value =
-      counters[index].value <= 0 ? 0 : counters[index].value - 1;
-    this.setState({
-      counters: counters,
-    });
-  };
-  handlingReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({
-      counters: counters,
-    });
-  };
-  handlingDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
   render() {
     return (
       <div>
         <button
-          onClick={this.handlingReset}
+          onClick={this.props.onReset}
           className="btn btn-sm btn-primary p-2 m-2 btn-block col-lg-4"
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onIncrement={this.handlingIncrement}
-            onDecrement={this.handlingDecrement}
-            onDelete={this.handlingDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
             counter={counter}
           ></Counter>
         ))}
