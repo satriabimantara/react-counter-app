e592bba9492d68abeb7314d01b1c878cd12eee5d
satriabimantara <satriabimantara.md@gmail.com>
2021-01-26 21:38:59 +0800

    Memindahkan state dari Counters agar bisa digunakan pada Navbar. Intinya memindahkan state dan method ke App root (Lifting Up)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import Counters from './components/Counters'
+import Navbar from './components/Navbar'
 import './App.css';
 
 class App extends React.Component{
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handlingIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = counter;
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+  handlingDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = counter;
+    counters[index].value =
+      counters[index].value <= 0 ? 0 : counters[index].value - 1;
+    this.setState({
+      counters: counters,
+    });
+  };
+  handlingReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({
+      counters: counters,
+    });
+  };
+  handlingDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
   render(){
     return(
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar totalCounters={this.state.counters.filter((c)=>c.value>0).length}/>
+        <main>
+          <Counters
+          counters = {this.state.counters}
+          onDelete = {this.handlingDelete}
+          onReset={this.handlingReset} 
+          onDecrement={this.handlingDecrement} 
+          onIncrement={this.handlingIncrement}/>
+        </main>
+      </React.Fragment>
     )
   }
 }
